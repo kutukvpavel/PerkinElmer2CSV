385c72794d0e619ceb2765a66b6a0ed413ac4f34
kutukvpavel <41666738+kutukvpavel@users.noreply.github.com>
2022-02-28 10:39:20 +0300

    Add helper projects

---
+++ CsvExportFixer/Fixers.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace CsvExportFixer
+{
+    public static class Fixers
+    {
+        /// <summary>
+        /// Use after <see cref="FixHeader(string)"/>
+        /// </summary>
+        /// <param name="contents"></param>
+        /// <returns></returns>
+        public static string FixCultureToInvariant(this string contents)
+        {
+            using StringReader r = new StringReader(contents);
+            using StringWriter w = new StringWriter();
+            w.WriteLine(r.ReadLine()); //Header
+            string line = null;
+            while ((line = r.ReadLine()) != null)
+            {
+                w.WriteLine(LineCultureHelper(line));
+            }
+            return w.ToString();
+        }
+        public static string FixHeader(this string contents)
+        {
+            using StringReader r = new StringReader(contents);
+            using StringWriter w = new StringWriter();
+            for (int i = 0; i < 3; i++)
+            {
+                r.ReadLine();
+            }
+            w.WriteLine(r.ReadLine());
+            r.ReadLine();
+            w.Write(r.ReadToEnd());
+            return w.ToString();
+        }
+
+
+        private static string LineCultureHelper(string l)
+        {
+            l = l.Replace(",00, ", ".00, ");
+            int i = l.IndexOf(',') + 1;
+            i = l.IndexOf(',', i);
+            if (i > -1) l = l.Remove(i, 1).Insert(i, ".");
+            return l;
+        }
+    }
+}
---
+++ CsvExportFixer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CsvExportFixer
+{
+    enum FileCondition
+    {
+        DoNotProcess,
+        Original,
+        SecondaryProcessing
+    }
+
+    /// <summary>
+    /// Intended to fix messed up CSV formatting in export files from TimeBase (yet)
+    /// Main export drawback: number format culture infot is ignored, resulting in mixed separators.
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> files = new List<string>();
+            SearchOption recursive = args.Contains("-r") ? SearchOption.AllDirectories : SearchOption.TopDirectoryOnly;
+            string searchPattern = args.FirstOrDefault(x => x.StartsWith("-e:")) ?? "*.csv";
+            foreach (var item in args)
+            {
+                if (File.Exists(item))
+                {
+                    files.Add(item);
+                }
+                else if (Directory.Exists(item))
+                {
+                    files.AddRange(Directory.GetFiles(item, searchPattern, recursive));
+                }
+            }
+            foreach (var item in files)
+            {
+                string contents = File.ReadAllText(item);
+                FileCondition c = MatchFile(contents, item);
+                if (c != FileCondition.DoNotProcess)
+                {
+                    File.WriteAllText(item, ProcessFile(contents, c));
+                    Console.WriteLine("Processed file: " + item);
+                }
+            }
+        }
+
+        static string ProcessFile(string contents, FileCondition c)
+        {
+            if (c == FileCondition.Original) contents = contents.FixHeader();
+            contents = contents.FixCultureToInvariant();
+            return contents;
+        }
+
+        static FileCondition MatchFile(string contents, string path)
+        {
+            if (path.Contains(".sp")) return FileCondition.DoNotProcess;
+            if (contents.StartsWith("\"Name:\",")) return FileCondition.Original;
+            if (contents.StartsWith("\"Wavenumber\",")) return FileCondition.SecondaryProcessing;
+            return FileCondition.DoNotProcess;
+        }
+    }
+}
